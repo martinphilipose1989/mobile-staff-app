340c06ba7c8abca39d95fb2182a4dcf7c986614d
AshrafNeo <ashraf.khan@neosoftmail.com>
2024-08-20 18:19:45 +0530

    added option for opacity

--- app/lib/utils/common_widgets/common_image_widget.dart
+++ app/lib/utils/common_widgets/common_image_widget.dart
@@ -3,16 +3,16 @@ import 'package:app/utils/common_widgets/app_images.dart';
 import 'package:flutter/material.dart';
 
 class CommonImageWidget extends StatelessWidget {
-  const CommonImageWidget({
-    super.key,
-    required this.imageUrl,
-    this.assetImageBoxFit = BoxFit.cover,
-    this.networkImageBoxFit = BoxFit.cover,
-    this.fallbackAssetImagePath = AppImages.defaultAvatar,
-    this.imageHeight,
-    this.imageWidth,
-    this.progressSize,
-  });
+  const CommonImageWidget(
+      {super.key,
+      required this.imageUrl,
+      this.assetImageBoxFit = BoxFit.cover,
+      this.networkImageBoxFit = BoxFit.cover,
+      this.fallbackAssetImagePath = AppImages.defaultAvatar,
+      this.imageHeight,
+      this.imageWidth,
+      this.progressSize,
+      this.showOpacity = false});
 
   final String imageUrl;
   final String fallbackAssetImagePath;
@@ -21,6 +21,7 @@ class CommonImageWidget extends StatelessWidget {
   final BoxFit networkImageBoxFit;
   final BoxFit assetImageBoxFit;
   final Size? progressSize;
+  final bool showOpacity;
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +29,9 @@ class CommonImageWidget extends StatelessWidget {
       imageUrl: imageUrl,
       width: imageWidth,
       height: imageHeight,
+      color: showOpacity ? Colors.white.withOpacity(0.5) : null,
+      colorBlendMode: showOpacity ? BlendMode.modulate : null,
+      memCacheHeight: (100 * MediaQuery.devicePixelRatioOf(context)).toInt(),
       fit: networkImageBoxFit,
       progressIndicatorBuilder: (context, url, downloadProgress) => Center(
         child: SizedBox(
@@ -40,12 +44,8 @@ class CommonImageWidget extends StatelessWidget {
         ),
       ),
       useOldImageOnUrlChange: true,
-      errorWidget: (context, url, error) => Image.asset(
-        fallbackAssetImagePath,
-        width: imageWidth,
-        height: imageHeight,
-        fit: assetImageBoxFit,
-      ),
+      errorWidget: (context, url, error) => Image.asset(fallbackAssetImagePath,
+          width: imageWidth, height: imageHeight, fit: assetImageBoxFit),
     );
   }
 }
